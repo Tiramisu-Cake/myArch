61544fa482add4bfa4227a725e6e02f392d85676
Tiramisu-Cake <igkharin@gmail.com>
2025-08-19 15:58:28 +0400

    nvidia drivers

---
+++ set_nvidia_drivers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo pacman -Syu
+sudo pacman -S nvidia-dkms nvidia-utils nvidia-settings nvidia-headers
+
+echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
+sudo mkinitcpio -P
+
+echo "You have to write the following options to the \'options' line of your /boot/loader/entries/*.conf:"
+echo "nvidia_drm.modeset=1 modprobe.blacklist=nouveau"
